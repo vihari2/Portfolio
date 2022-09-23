44202d35ad3da07d66620ff886e26066110e32ba
vihari2 <freitasvitoria712@gmail.com>
2022-09-23 18:19:14 -0300

    ;

--- middleware/auth.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-export default function ({ store, redirect }) {
-  // se o usuario estiver não logado redireciona para a dashboard
-  if (store.state.auth.loggedIn === false) return redirect('/login');
-}
--- middleware/guest.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-export default function ({ store, redirect }) {
-  // se o usuario estiver logado redireciona para a dashboard
-  if (store.state.auth.loggedIn === true) return redirect('/dashboard');
-}
